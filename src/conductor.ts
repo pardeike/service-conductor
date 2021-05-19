e8ac69dcbfac4c3e1cb91dad8bc04a6113725bdb
Andreas Pardeike <andreas@pardeike.net>
2021-05-19 15:39:10 +0200

    process methods+functions

--- src/conductor.ts
+++ src/conductor.ts
@@ -1,6 +1,6 @@
 import ts from 'typescript'
 
-function setBody(factory: ts.NodeFactory, node: ts.MethodDeclaration, body: ts.Block): ts.MethodDeclaration {
+function setMethodBody(factory: ts.NodeFactory, node: ts.MethodDeclaration, body: ts.Block): ts.MethodDeclaration {
 	return factory.updateMethodDeclaration(
 		node,
 		node.decorators,
@@ -15,9 +15,46 @@ function setBody(factory: ts.NodeFactory, node: ts.MethodDeclaration, body: ts.B
 	)
 }
 
+function setFunctionBody(factory: ts.NodeFactory, node: ts.FunctionDeclaration, body: ts.Block): ts.FunctionDeclaration {
+	return factory.updateFunctionDeclaration(
+		node,
+		node.decorators,
+		node.modifiers,
+		node.asteriskToken,
+		node.name,
+		node.typeParameters,
+		node.parameters,
+		node.type,
+		body,
+	)
+}
+
+function update(f: ts.NodeFactory, sourceFile: ts.SourceFile, node: ts.MethodDeclaration | ts.FunctionDeclaration): ts.Block {
+	return f.createBlock(
+		f.createNodeArray(
+			[].concat(
+				f.createCallExpression(
+					f.createPropertyAccessExpression(
+						f.createPropertyAccessExpression(
+							f.createIdentifier('service_conductor_1'),
+							f.createIdentifier('default'),
+						),
+						f.createIdentifier('run'),
+					),
+					undefined,
+					[f.createStringLiteral(`${node.name.getText(sourceFile)}`)],
+				),
+				node.body.statements,
+			),
+			false,
+		),
+	)
+}
+
 export function transformer(context: ts.TransformationContext): ts.Transformer<ts.SourceFile> {
 	const f = context.factory
 	function transform(sourceFile: ts.SourceFile): ts.SourceFile {
+
 		let firstTime = true
 		const visitImport = (node: ts.Node): ts.VisitResult<ts.Node> => {
 			if (ts.isSourceFile(node) == false && firstTime) {
@@ -44,26 +81,12 @@ export function transformer(context: ts.TransformationContext): ts.Transformer<t
 
 		const visitCall = (node: ts.Node): ts.VisitResult<ts.Node> => {
 			if (ts.isMethodDeclaration(node)) {
-				const body = f.createBlock(
-					f.createNodeArray(
-						[].concat(
-							f.createCallExpression(
-								f.createPropertyAccessExpression(
-									f.createPropertyAccessExpression(
-										f.createIdentifier('service_conductor_1'),
-										f.createIdentifier('default'),
-									),
-									f.createIdentifier('run'),
-								),
-								undefined,
-								[f.createStringLiteral(`${node.name.getText(sourceFile)}`)],
-							),
-							node.body.statements,
-						),
-						false,
-					),
-				)
-				return setBody(f, node, body)
+				const body = update(f, sourceFile, node)
+				return setMethodBody(f, node, body)
+			}
+			if (ts.isFunctionDeclaration(node)) {
+				const body = update(f, sourceFile, node)
+				return setFunctionBody(f, node, body)
 			}
 			return ts.visitEachChild(node, (child) => visitCall(child), context)
 		}
