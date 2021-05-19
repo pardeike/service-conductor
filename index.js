9426b5e85c1056421da70fb772749b70b3002172
Andreas Pardeike <andreas@pardeike.net>
2021-05-19 14:17:49 +0200

    fixes

--- index.js
+++
@@ -1,8 +0,0 @@
-console.log('Service Conductor initializing...')
-
-module.exports = {
-	run: (id) => {
-		console.log(`=== ${id} =========`)
-	},
-	transformer: require('./transformers/conductor').conductorTransformer
-}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log('Service Conductor initializing...')
+
+export function run(id: string) {
+	console.log(`=== ${id} =========`)
+}
