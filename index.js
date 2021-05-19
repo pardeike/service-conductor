2e39322933013bc5bcf04aa1e9635fe656a1b2f8
Andreas Pardeike <andreas@pardeike.net>
2021-05-19 14:04:38 +0200

    exports transformer

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ console.log('Service Conductor initializing...')
 module.exports = {
 	run: (id) => {
 		console.log(`=== ${id} =========`)
-	}
+	},
+	transformer: require('./transformers/conductor').conductorTransformer
 }
